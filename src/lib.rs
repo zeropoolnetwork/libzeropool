c756fb03d5d6988106c2b5e843f4b2530e5e32fb
Igor Gulamov <igor.gulamov@gmail.com>
2021-07-13 08:07:29 +0300

    replace poseidon seedbox with the reference

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 pub extern crate fawkes_crypto;
 
+use serde_json;
 
 pub mod constants;
 pub mod native;
@@ -15,22 +16,23 @@ use fawkes_crypto::native::poseidon::PoseidonParams;
 
 use lazy_static::lazy_static;
 
-fn gen_poseidon_params(n:usize) -> PoseidonParams<Fr> {
-    let t = n+1;
-    let r_p =  f64::ceil(53.75 + 1.075*f64::log(t as f64, 5.0)) as usize;
-    PoseidonParams::<Fr>::new(t, 8, r_p)
-}
 
-
-//TODO recalculate
 lazy_static! {
-    pub static ref POOL_PARAMS: PoolBN256 = PoolBN256 {
-        jubjub: JubJubBN256::new(),
-        hash: gen_poseidon_params(1),
-        compress: gen_poseidon_params(2),
-        note: gen_poseidon_params(4),
-        account: gen_poseidon_params(5),
-        eddsa: gen_poseidon_params(3),
-        sponge: gen_poseidon_params(5),
+    pub static ref POOL_PARAMS: PoolBN256 = {
+        let poseidon_params_t_2:PoseidonParams<Fr> = serde_json::from_str(include_str!("../res/poseidon_params_t_2.json")).unwrap();
+        let poseidon_params_t_3:PoseidonParams<Fr> = serde_json::from_str(include_str!("../res/poseidon_params_t_3.json")).unwrap();
+        let poseidon_params_t_4:PoseidonParams<Fr> = serde_json::from_str(include_str!("../res/poseidon_params_t_4.json")).unwrap();
+        let poseidon_params_t_5:PoseidonParams<Fr> = serde_json::from_str(include_str!("../res/poseidon_params_t_5.json")).unwrap();
+        let poseidon_params_t_6:PoseidonParams<Fr> = serde_json::from_str(include_str!("../res/poseidon_params_t_6.json")).unwrap();
+        
+        PoolBN256 {
+            jubjub: JubJubBN256::new(),
+            hash: poseidon_params_t_2.clone(),
+            compress: poseidon_params_t_3,
+            note: poseidon_params_t_5,
+            account: poseidon_params_t_6.clone(),
+            eddsa: poseidon_params_t_4,
+            sponge: poseidon_params_t_6,
+        }
     };
 }

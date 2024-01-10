323dfcc524a6dccb2866877ab12023db2226b1d0
Dmitry Vdovin <voidxnull@gmail.com>
2024-01-10 13:49:31 +0400

    Fix plonk setup

--- src/setup/cli_plonk.rs
+++ src/setup/cli_plonk.rs
@@ -1,11 +1,16 @@
 use std::fs::File;
+use std::io::Write;
 
 use clap::Clap;
-use fawkes_crypto::{
-    backend::plonk::{Parameters, engines::Bn256},
-    circuit::cs::CS,
-};
-use halo2_proofs::poly::commitment::Params;
+use fawkes_crypto::{backend::plonk::{Parameters, engines::Bn256}, BorshSerialize, circuit::cs::CS};
+use fawkes_crypto::backend::plonk::setup::setup;
+use fawkes_crypto::engines::bn256::Fr;
+use near_halo2_verifier::bn256::G1Affine;
+use near_halo2_verifier::loader::NearLoader;
+use near_halo2_verifier::PlonkVerifierData;
+use libzeropool::circuit::tree::{CTreePub, CTreeSec, tree_update};
+use libzeropool::circuit::tx::{c_transfer, CTransferPub, CTransferSec};
+use libzeropool::POOL_PARAMS;
 
 pub fn main() {
     let opts: Opts = Opts::parse();
@@ -14,14 +19,43 @@ pub fn main() {
     }
 }
 
+fn tree_circuit<C:CS<Fr=Fr>>(public: CTreePub<C>, secret: CTreeSec<C>) {
+    tree_update(&public, &secret, &*POOL_PARAMS);
+}
+
+fn tx_circuit<C:CS<Fr=Fr>>(public: CTransferPub<C>, secret: CTransferSec<C>) {
+    c_transfer(&public, &secret, &*POOL_PARAMS);
+}
+
 fn cli_setup(o:SetupOpts) {
     let params_path = o.params.unwrap_or("plonk_params.bin".to_string());
-    let params: Parameters<Bn256> = Parameters::setup(o.k);
 
-    let mut fp = File::create(params_path).unwrap();
-    Params::write(&params.0, &mut fp).unwrap();
+    let params = if !std::path::Path::new(&params_path).exists() {
+        let params: Parameters<Bn256> = Parameters::setup(o.k);
+        println!("setup OK");
+        let mut fp = File::create(params_path).unwrap();
+        params.write(&mut fp).unwrap();
+        params
+    } else {
+        let mut fp = File::open(params_path).unwrap();
+        Parameters::read(&mut fp).unwrap()
+    };
 
-    println!("setup OK");
+    let (vk, _) = setup(&params, tree_circuit);
+    let tree_vd = PlonkVerifierData::new(params.0.clone(), vk.0, o.k);
+    let tree_vd_bytes = tree_vd.try_to_vec().unwrap();
+    let mut fp = File::create("tree_vd.bin").unwrap();
+    fp.write_all(&tree_vd_bytes).unwrap();
+
+    println!("tree OK");
+
+    let (vk, _) = setup(&params, tx_circuit);
+    let tx_vd = PlonkVerifierData::new(params.0.clone(), vk.0, o.k);
+    let tx_vd_bytes = tx_vd.try_to_vec().unwrap();
+    let mut fp = File::create("tx_vd.bin").unwrap();
+    fp.write_all(&tx_vd_bytes).unwrap();
+
+    println!("tx OK");
 }
 
 #[derive(Clap)]
